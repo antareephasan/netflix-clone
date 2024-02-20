ad82628de31b4f52821901e8ad7dee366cc8c013
antareephasan <antareephasan@gmail.com>
2024-02-21 03:52:31 +0600

    movielist and moviecard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
-  if(!session) {
+  if (!session) {
     return {
       redirect: {
         destination: '/auth',
@@ -21,11 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-
+  const { data: movies } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}  />
+      </div>
     </>
   )
 }
