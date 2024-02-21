95808883a36e1d8da432f3f39e8964951872f60e
antareephasan <antareephasan@gmail.com>
2024-02-21 12:28:08 +0600

    added: infomodal

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,9 +1,12 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import serverAuth from "@/lib/serverAuth";
 import { signOut } from "next-auth/react";
 
 interface AccountMenuProps {
     visible?: boolean;
 }
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const { data } = useCurrentUser();
     if (!visible) {
         return null;
     }
@@ -14,7 +17,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-blue.png" alt="profile" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        Username
+                        {data?.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
+  
 
   if (!session) {
     return {
@@ -26,8 +29,10 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies } = useMovieList();
   const { data: favorites } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
