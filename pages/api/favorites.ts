512c5e7faf83342b73335116e39f006f875b0045
antareephasan <antareephasan@gmail.com>
2024-02-21 12:44:59 +0600

    v1: final commit

--- pages/api/favorites.ts
+++ pages/api/favorites.ts
@@ -2,7 +2,7 @@ import serverAuth from "@/lib/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
 import prismadb from '@/lib/prismadb';
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         return res.status(405).end();
     }
